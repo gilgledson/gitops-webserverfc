24b6235d7e394c2b7aadbf23f87820b82e0a1260
Gilgledson <gilgledson96@gmail.com>
2023-12-12 11:55:33 -0300

    commit init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gilgledson/webserverfc
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>Hellow full cycle</h1>"))
+	})
+	http.ListenAndServe(":8001", nil)
+}
