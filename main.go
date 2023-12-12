b3cd03d58b43f39a653fb394dad6effd87680d35
Gilgledson <gilgledson96@gmail.com>
2023-12-12 14:20:49 -0300

    fix: description teste fix bug 0001

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hellow full cycle 02</h1>"))
+		w.Write([]byte("<h1>Hellow full cycle 03</h1>"))
 	})
 	http.ListenAndServe(":8001", nil)
 }
